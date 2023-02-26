f4a4e67611e7817472bdca6c40adad60fa22582a
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2023-02-26 21:43:14 +0200

    any

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,27 @@ import ProblemsetCreatorPage from "./components/problemsetcreator";
 import StandingPage from "./components/standig-list";
 import "./styles/App.css";
 function App() {
+  const home = (
+    <div>
+      <h1>It works Fine ✌</h1>
+      <ul>
+        <li>
+          <a href="/problemset">problemset</a>
+        </li>
+        <li>
+          <a href="/standing">standing</a>
+        </li>
+      </ul>
+    </div>
+  );
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/problemset" exact element={<ProblemsetCreatorPage />} />
-          <Route path="/standing" exact element={<StandingPage />} />
+          <Route path="/problemset" element={<ProblemsetCreatorPage />} />
+          <Route path="/standing" element={<StandingPage />} />
+          <Route path="/" element={<></>} />
         </Routes>
       </Router>
     </div>
